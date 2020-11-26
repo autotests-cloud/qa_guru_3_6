bc86e6f854d9f656d1755cf69d4561010cef4787
svasenkov <aanher@gmail.com>
2020-11-26 21:39:02 +0300

    With JavaFaker

---
+++ src/test/java/tests/StudentRegistrationFormWithJavaFakerTests.java
@@ -0,0 +1,129 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.Locale;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static utils.RandomUtils.*;
+
+public class StudentRegistrationFormWithJavaFakerTests {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void successfulFillFormTest() {
+        Faker faker = new Faker();
+        FakeValuesService fakeValuesService = new FakeValuesService(
+                new Locale("en-GB"), new RandomService());
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String email = fakeValuesService.bothify("????##@gmail.com");
+        String mobile = fakeValuesService.regexify("[0-9]{10}");
+        String currentAddress = faker.address().fullAddress();
+
+        String
+                gender = "Other",
+                dayOfBirth = "10",
+                monthOfBirth = "May",
+                yearOfBirth = "1988",
+                subject1Prefix = "m",
+                subject1 = "Chemistry",
+                subject2Prefix = "e",
+                subject2 = "Commerce",
+                hobby1 = "Sports",
+                hobby2 = "Reading",
+                hobby3 = "Music",
+                picture = "1.png",
+                state = "Uttar Pradesh",
+                city = "Merrut";
+
+
+        open("https://demoqa.com/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        $("#firstName").val(firstName);
+        $("#lastName").val(lastName);
+        $("#userEmail").val(email);
+//        $(byText(gender)).click();
+        $("#genterWrapper").$(byText(gender)).click();
+        $("#userNumber").val(mobile);
+        // set date
+        $("#dateOfBirthInput").clear();
+        $(".react-datepicker__month-select").selectOption(monthOfBirth);
+        $(".react-datepicker__year-select").selectOption(yearOfBirth);
+        $(".react-datepicker__day--0" + dayOfBirth).click();
+        // set subject
+        $("#subjectsInput").val(subject1Prefix);
+        $(".subjects-auto-complete__menu-list").$(byText(subject1)).click();
+        $("#subjectsInput").val(subject2Prefix);
+        $(".subjects-auto-complete__menu-list").$(byText(subject2)).click();
+        // set hobbies
+        $("#hobbiesWrapper").$(byText(hobby1)).click();
+        $("#hobbiesWrapper").$(byText(hobby2)).click();
+        $("#hobbiesWrapper").$(byText(hobby3)).click();
+
+        $("#uploadPicture").uploadFile(new File("src/test/resources/" + picture));
+
+        $("#currentAddress").val(currentAddress);
+
+        // set state and city
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText(state)).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText(city)).click();
+
+        $("#submit").click();
+
+        // asserts
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+//        $(".table-responsive").shouldHave(text(firstName + " " + lastName),
+//                text(email), text(gender));
+        $x("//td[text()='Student Name']").parent().shouldHave(text(firstName + " " + lastName));
+        $x("//td[text()='Student Email']").parent().shouldHave(text(email));
+        $x("//td[text()='Gender']").parent().shouldHave(text(gender));
+        $x("//td[text()='Mobile']").parent().shouldHave(text(mobile + ""));
+        $x("//td[text()='Date of Birth']").parent().shouldHave(text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth));
+        $x("//td[text()='Subjects']").parent().shouldHave(text(subject1 + ", " + subject2));
+        $x("//td[text()='Hobbies']").parent().shouldHave(text(hobby1 + ", " + hobby2 + ", " + hobby3));
+        $x("//td[text()='Picture']").parent().shouldHave(text(picture));
+        $x("//td[text()='Address']").parent().shouldHave(text(currentAddress));
+        $x("//td[text()='State and City']").parent().shouldHave(text(state + " " + city));
+
+    }
+
+    @Test
+    void alfaTest() {
+        open("https://alfabank.ru/make-money/");
+
+        $("[data-test-id=tabs-list-tabTitle-0]").sibling(0).click();
+        // sibling(), preceding(), parent(), closest()
+
+        $(byText("Альфа-Банк является участником системы обязательного страхования вкладов")).shouldBe(visible);
+    }
+
+    static void fillDefaultDateOfBirth() {
+        fillDateOfBirth("10", "20", "1900");
+    }
+
+    static void fillDateOfBirth(String dayOfBirth, String monthOfBirth, String yearOfBirth){
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(monthOfBirth);
+        $(".react-datepicker__year-select").selectOption(yearOfBirth);
+        $(".react-datepicker__day--0" + dayOfBirth).click();
+    }
+
+
+}
